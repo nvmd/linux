c7f976851f68e265d14b4467b5d88a4049709aee
Sergey Kazenyuk <kazenyuk@gmail.com>
2012-06-01 18:54:27 +0400

    Create pslist structure for early-bird processes.

--- kernel/sysfs_pslist.c
+++ kernel/sysfs_pslist.c
@@ -11,7 +11,6 @@ static ssize_t process_name_show(struct kobject *kobj,
 	strncpy(buf, container_of(kobj, struct task_struct, pslist_link)->comm,
 		TASK_COMM_LEN);
 
-	//snprintf(buf, PAGE_SIZE, "%." stringify(TASK_COMM_SIZE) "s",   
 	return TASK_COMM_LEN;
 }
 
@@ -46,14 +45,16 @@ int pslist_task_init(struct task_struct *child)
 	
 	memset(&child->pslist_link, 0, sizeof(struct kobject));
 
-	if (pslist_root_kobj.state_initialized) {
-		kobject_init(&child->pslist_link, &process_kobj_type);
-		result = kobject_add(&child->pslist_link, &pslist_root_kobj, "%d", child->pid);
-		if (result) {
-			printk(KERN_ERR "Can't add child process to the pslist tree\n");
-			kobject_put(&child->pslist_link);
-			return result;
-		}
+	if (!pslist_root_kobj.state_initialized) {
+		return 0;
+	}
+
+	kobject_init(&child->pslist_link, &process_kobj_type);
+	result = kobject_add(&child->pslist_link, &pslist_root_kobj, "%d", child->pid);
+	if (result) {
+		printk(KERN_ERR "Can't add child process to the pslist tree\n");
+		kobject_put(&child->pslist_link);
+		return result;
 	}
 
 	return 0;
@@ -62,14 +63,17 @@ int pslist_task_init(struct task_struct *child)
 int pslist_task_link(struct task_struct *parent, struct task_struct *child)
 {
 	int result;
-	if (pslist_root_kobj.state_initialized) {
-		result = sysfs_create_link(&parent->pslist_link, 
-					&child->pslist_link, 
-					kobject_name(&child->pslist_link));
-		if (result) {
-			printk(KERN_ERR "Can't create sysfs link between child and parent processes");
-			return result;
-		}
+
+	if (!pslist_root_kobj.state_initialized) {
+		return 0;
+	}
+
+	result = sysfs_create_link(&parent->pslist_link,
+				&child->pslist_link,
+				kobject_name(&child->pslist_link));
+	if (result) {
+		printk(KERN_ERR "Can't create sysfs link between child and parent processes\n");
+		return result;
 	}
 
 	return 0;
@@ -79,10 +83,18 @@ void pslist_task_release(struct task_struct *tsk)
 {
 	// tsk->pslist_link can be uninitialized in case the task 
 	// is created and relased before pslist initialization
-	if (tsk->pslist_link.state_initialized) {
-		kobject_del(&tsk->pslist_link);
-		kobject_put(&tsk->pslist_link);
+	if (!tsk->pslist_link.state_initialized) {
+		return;
 	}
+
+	if (tsk->real_parent) {
+		sysfs_delete_link(&tsk->real_parent->pslist_link,
+				&tsk->pslist_link,
+				kobject_name(&tsk->pslist_link));
+	}
+
+	kobject_del(&tsk->pslist_link);
+	kobject_put(&tsk->pslist_link);
 }
 
 /**
@@ -91,6 +103,7 @@ void pslist_task_release(struct task_struct *tsk)
 int __init pslist_ksysfs_init(void)
 {
 	int result;
+	struct task_struct *task;
 
 	kobject_init(&pslist_root_kobj, &pslist_root_kobj_type);
 	result = kobject_add(&pslist_root_kobj, kernel_kobj, "pslist");
@@ -100,7 +113,21 @@ int __init pslist_ksysfs_init(void)
 		return result;
 	}
 
-	// TODO: create ksysfs nodes for processes started earlier
+	rcu_read_lock();
+	printk(KERN_DEBUG "Creating pslist ksysfs structure for early-bird processes\n");
+	// create ksysfs nodes for early-bird processes
+	for_each_process(task) {
+		pslist_task_init(task);
+	}
+
+	// setup parent-child links for early-birds
+	for_each_process(task) {
+                if (task->real_parent) {
+                        pslist_task_link(task->real_parent, task);
+                }
+        }
+	printk(KERN_DEBUG "Finished creating pslist ksysfs structure for early-bird processes\n");
+	rcu_read_unlock();
 
 	return 0;
 }
